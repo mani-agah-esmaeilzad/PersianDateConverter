4f69e25df1a22a01b3a1b759c6bf2564d5d06127
mani-agah <m.s.agah@outlook.com>
2024-02-16 23:45:54 +0330

    first commit

---
+++ PersianDateConverter/Converter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace PersianDateConverter
+{
+    public class Converter
+    {
+        private static PersianCalendar pc = new PersianCalendar();
+
+        public static DateTime ToGregorian(string persianDate)
+        {
+            string[] parts = persianDate.Split('/');
+            int year = int.Parse(parts[0]);
+            int month = int.Parse(parts[1]);
+            int day = int.Parse(parts[2]);
+            return pc.ToDateTime(year, month, day, 0, 0, 0, 0);
+        }
+
+        public static string ToPersian(DateTime date)
+        {
+            int year = pc.GetYear(date);
+            int month = pc.GetMonth(date);
+            int day = pc.GetDayOfMonth(date);
+            return $"{year}/{month}/{day}";
+        }
+
+        public static bool IsValidPersianDate(string persianDate)
+        {
+            try
+            {
+                ToGregorian(persianDate);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
